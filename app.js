3e35f94730b59300ece634211705650f8d25cf56
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-06-22 14:16:48 +0100

    Clear depracation warnings

--- app.js
+++ app.js
@@ -3,23 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv/config');
-
 app.use(bodyParser.json());
-
 // Import Routes 
 const postsRoute = require('./routes/posts');
-
 app.use('/posts', postsRoute);
-
 // ROUTES 
 app.get('/', (req, res) => {
-    res.send('We are on home');
+	res.send('We are on home');
 });
-
 // Connect to DB 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParse: true }, () =>
-  console.log("Connected to the DB!")
+mongoose.connect(process.env.DB_CONNECTION, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}, () =>
+	console.log("Connected to the DB!")
 );
-
-
 app.listen(4000);
